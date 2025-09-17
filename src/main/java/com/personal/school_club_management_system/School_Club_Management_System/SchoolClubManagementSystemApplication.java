b8eac54ae57b927ed178c86a5e426d8a544928ee
hanswys <123027447+hanswys@users.noreply.github.com>
2025-09-17 15:17:26 -0500

    init commit

---
+++ src/main/java/com/personal/school_club_management_system/School_Club_Management_System/SchoolClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.personal.school_club_management_system.School_Club_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolClubManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolClubManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/school_club_management_system/School_Club_Management_System/SchoolClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.school_club_management_system.School_Club_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolClubManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
